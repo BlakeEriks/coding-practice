f7f9acdc03881994bc1cc10b9bec8692e487515a
Blake Eriks <eblake98@vt.edu>
2021-08-21 10:16:38 -1000

    added findUniqueNum.js

---
+++ codewars/kyu-6/findUniqueNum.js
@@ -0,0 +1,13 @@
+function findUniq(arr) {
+    numSet = {};
+    for ( let i = 0; i < arr.length-2; i++ ) {
+        let cur = arr[i], next = arr[i+1], nextnext = arr[i+2];
+        if (cur === next && cur === nextnext) continue;
+        if (cur === next) return nextnext;
+        if (cur === nextnext) return next;
+        return cur;
+    }
+    return null;
+}
+
+console.log(findUniq([1,1,0.55,1,1,1]));
