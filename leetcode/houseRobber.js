5d2967e898fa411aca65c93ce1ba8350040ce036
Blake Eriks <eblake98@vt.edu>
2022-03-02 17:40:35 -0800

    Add recursive solution

--- leetcode/houseRobber.js
+++ leetcode/houseRobber.js
@@ -2,14 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-  if (nums.length < 3) return Math.max(nums[0], nums[1] || 0)
-  let dp = new Array(nums.length).fill(0)
-  dp[0] = nums[0]
-  dp[1] = nums[1]
-  dp[2] = nums[2] + nums[0]
-  for (let i = 3; i < dp.length; i++) {
-      dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i]
+var robRecursiveMemo = function(nums) {
+  
+  let memo = new Array(nums.length).fill(-1)
+
+  const robRecursive = index => {
+    if (index < 0) return 0
+    if (memo[index] >= 0) return memo[index]
+    let res = Math.max(robRecursive(index - 2) + nums[index], robRecursive(index - 1))
+    memo[index] = res
+    return res
   }
-  return Math.max(dp[dp.length - 1], dp[dp.length - 2])
+
+  return robRecursive(nums.length - 1)
+};
+
+var robIterativeDP = function(nums) {
+  
+  let dp = new Array(nums.length).fill(-1)
+  dp[0] = 0
+  dp[1] = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i])
+  }
+
+  return dp[nums.length]
 };
