20e078e62400b636e239dcc9ef886ab70e3c2bbd
Blake Eriks <eblake98@vt.edu>
2022-03-12 14:54:50 -0800

    Complete mergeTwoLinkedLists recursive

--- leetcode/mergeTwoLinkedLists.js
+++ leetcode/mergeTwoLinkedLists.js
@@ -10,7 +10,7 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
+var mergeTwoListsIterative = function(list1, list2) {
   if (!list1) return list2
   if (!list2) return list1
 
@@ -45,3 +45,16 @@ var mergeTwoLists = function(list1, list2) {
   }
   return head
 };
+
+var mergeTwoLinkedListsRecursive = function(list1, list2) {
+  if (!list1) return list2
+  if (!list2) return list1
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLinkedListsRecursive(list1.next, list2)
+    return list1
+  }
+  else {
+    list2.next = mergeTwoLinkedListsRecursive(list1, list2.next)
+    return list2
+  }
+}
